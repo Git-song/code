3ee6707f6813ed50102e68fa51f55f6311831d77
Git-song <1593673623@qq.com>
2019-10-18 18:27:13 +0800

    upto new code

--- c_code/chapter_three/code.c
+++ c_code/chapter_three/code.c
@@ -1,16 +1,41 @@
 #include "stdio.h"
 
-/***********第1题***********/
+/***********第2题***********/
+void practice2()
+{
+    int Ascii = 0;
+    printf("please enter the Value of ASCII:");
+    scanf("%d",&Ascii);
+    printf("\nyour enter value is:%d",Ascii);
+    printf("\nthe character:%c.\n",(char)Ascii);
+    getchar();
+}
 
+/************第3题**************/
+void practice3()
+{
+    printf("\a\nStartled by the sund, Sally shouted,\n");
+    printf("\"By the Great Pumpkin, what was that!\"");
+}
+
+/***********第4题****************/
+void practice4()
+{
+    float fl;
+    printf("Enter a floating-point value: ");
+    scanf("%f",&fl);
+    printf("\nfixed-ponit notation: %f",fl);
+    printf("\nexponential notation: %e",fl);
+    printf("\np notation: %a",fl);
+    getchar();
+}
 
 int main(void)
 {
-    int count;
-    count = sizeof(long);
-
-    printf("%d",count);
-
-
+    practice2();
+    practice3();
+    practice4();
+    
     getchar();
     return 0;
 }
